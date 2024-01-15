04419ed6ef9607a10b25f82426be63a05d57851c
Sahid <mscode298@gmail.com>
2024-01-14 23:03:00 -0500

    Array Practice

--- src/mscode/arrays/ArraysTopic.java
+++ src/mscode/arrays/ArraysTopic.java
@@ -8,6 +8,11 @@ public class ArraysTopic {
         //How to create an Array
         //dataType variables[] = new dataType[Size of Array]
         //Data type in Array should be same
+        // int n[5]; error since Arrays are reference type
+        // in java memory allocation for reference data type is done during runtime using new operator
+        //index starts from 0
+        // Array initialization
+        //int[] n = {10, 20, 30, 40, 50}
 
         int[] marks = new int[3];
         marks[0]= 98;
